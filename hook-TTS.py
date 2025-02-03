3a1a90b52203a552fec4c342e2d56caef9271bc0
overcrash <3681221+overcrash66@users.noreply.github.com>
2025-02-03 18:14:08 -0400

    Update hook-TTS.py

--- hook-TTS.py
+++ hook-TTS.py
@@ -1,9 +1,13 @@
 # hook-TTS.py
 from PyInstaller.utils.hooks import collect_all, collect_data_files, collect_submodules
-import typeguard._decorators
-typeguard._decorators.instrument = lambda func, **kwargs: func
-from TTS.api import TTS
+from PyInstaller.utils.hooks import collect_all
+
+datas, binaries, hiddenimports = collect_all('inflect')
 
 datas, binaries, hiddenimports = collect_all('TTS')
+
 datas += collect_data_files('TTS')
 hiddenimports += collect_submodules('TTS')
+
+datas = collect_data_files('typeguard')
+hiddenimports = ['typeguard._decorators', 'typeguard._importhook']
