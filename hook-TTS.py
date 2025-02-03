5bde3e0258cd1e10d5ecbdffc232d4b123269072
overcrash <3681221+overcrash66@users.noreply.github.com>
2025-02-03 12:12:14 -0400

    Update hook-TTS.py

--- hook-TTS.py
+++ hook-TTS.py
@@ -1,4 +1,6 @@
 # hook-TTS.py
-from PyInstaller.utils.hooks import collect_all
+from PyInstaller.utils.hooks import collect_all, collect_data_files, collect_submodules
 
 datas, binaries, hiddenimports = collect_all('TTS')
+datas += collect_data_files('TTS')
+hiddenimports += collect_submodules('TTS')
