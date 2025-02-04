03050beaac868edc4c726c6c326ee53d17dc1cde
overcrash <3681221+overcrash66@users.noreply.github.com>
2025-02-03 20:19:44 -0400

    Update hook-TTS.py

--- hook-TTS.py
+++ hook-TTS.py
@@ -1,13 +1,26 @@
 # hook-TTS.py
 from PyInstaller.utils.hooks import collect_all, collect_data_files, collect_submodules
-from PyInstaller.utils.hooks import collect_all
+from PyInstaller.utils.hooks import get_package_paths
 
-datas, binaries, hiddenimports = collect_all('inflect')
+def hook(hook_api):
+    package_path = get_package_paths('inflect')[0]
+    hook_api.add_datas([(package_path, 'inflect')])
+    hiddenimports = ['inflect.engine', 'inflect.__init__']
+    hook_api.add_imports(*hiddenimports)
+    package_path = get_package_paths('TTS')[0]
+    hook_api.add_datas([(package_path, 'TTS')])
+    hiddenimports = ['TTS.engine', 'TTS.__init__']
+    hook_api.add_imports(*hiddenimports)
+    package_path = get_package_paths('typeguard')[0]
+    hook_api.add_datas([(package_path, 'typeguard')])
+    hiddenimports = ['typeguard._decorators', 'typeguard._importhook']
+    hook_api.add_imports(*hiddenimports)
+    return hook_api
 
-datas, binaries, hiddenimports = collect_all('TTS')
+infl_datas, infl_binaries, infl_hiddenimports = collect_all('inflect')
+tts_datas, tts_binaries, tts_hiddenimports = collect_all('TTS')
+typeguard_datas = collect_data_files('typeguard')
 
-datas += collect_data_files('TTS')
-hiddenimports += collect_submodules('TTS')
-
-datas = collect_data_files('typeguard')
-hiddenimports = ['typeguard._decorators', 'typeguard._importhook']
+datas = infl_datas + tts_datas + typeguard_datas
+binaries = infl_binaries + tts_binaries
+hiddenimports = infl_hiddenimports + tts_hiddenimports + collect_submodules('TTS') + ['typeguard._decorators', 'typeguard._importhook']
